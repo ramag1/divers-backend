021a3d052a52e537e86dbc09bb0051ec5ba78663
rmagley <rachelmagley@gmail.com>
2022-02-11 20:26:13 -0500

    Create site and visitor models

--- dive_django/dive/models.py
+++ dive_django/dive/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    name = models.CharField(max_length=100)
+    country= models.CharField(max_length=100)
+    max_depth = models.IntegerField()
+    site_type= models.CharField(max_length=100)
+    marine_life=models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+class Visitor(models.Model):
+    site = models.ForeignKey(Site, on_delete= models.CASCADE, related_name='visitors')
+    name = models.CharField(max_length=100)
+    visited = models.BooleanField()
+    favorite = models.BooleanField()
+    bucket_list = models.BooleanField()
+
+    def __str__(self):
+        return self.name
